0709ebea48dc2827d47a43a85778f67234cbc1ea
LoneWanderer-lab <118936466+LoneWanderer-lab@users.noreply.github.com>
2023-02-06 09:17:57 -0800

    Update main.py

--- YT shorts bot/main.py
+++ YT shorts bot/main.py
@@ -1,6 +1,7 @@
 from program_assets.cleanup import cleanup
 from program_assets.videoEditor import makeVideo
 from program_assets.memeDownloader import downloadMemes
+from program_assets.videoUploader import uploadVideo
 
 downloadMemes()
 makeVideo()
